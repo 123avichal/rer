ba0a5e5f6d3ad3d1ce9e78faebb5b2f860920b9f
123avichal <saxenaavichal@gmail.com>
2023-10-28 03:00:33 +0530

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+# Load the data
+data = []
+with open('data.json') as f:
+    data = json.load(f)
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+# Define the recommendation function
+def recommend_content(user_id):
+    # Get the user's preferences
+    user_preferences = data[user_id]['preferences']
+
+    # Find other users with similar preferences
+    similar_users = []
+    for user in data:
+        if user['preferences'] == user_preferences:
+            similar_users.append(user)
+
+    # Recommend content to the user
+    recommended_content = []
+    for user in similar_users:
+        for item in user['recommended_content']:
+            if item not in user_recommended_content:
+                recommended_content.append(item)
+
+    return recommended_content
+
+# Create the route for getting recommendations
+@app.route('/recommendations/<int:user_id>')
+def get_recommendations(user_id):
+    # Get the recommendations
+    recommendations = recommend_content(user_id)
+
+    # Return the recommendations as JSON
+    return jsonify(recommendations)
+
+if __name__ == '__main__':
+    app.run(debug=True)
